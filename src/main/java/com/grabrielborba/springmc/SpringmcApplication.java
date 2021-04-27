82ebbd96468fde60c87441a9236546a6c8f0c19f
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2021-04-27 10:39:32 -0300

    Criando operacao de instanciacao

--- src/main/java/com/grabrielborba/springmc/SpringmcApplication.java
+++ src/main/java/com/grabrielborba/springmc/SpringmcApplication.java
@@ -1,13 +1,32 @@
 package com.grabrielborba.springmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.grabrielborba.springmc.domain.Categoria;
+import com.grabrielborba.springmc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringmcApplication {
+public class SpringmcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
 }
